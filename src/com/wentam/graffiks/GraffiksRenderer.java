d585fdb8160e0c007280546f16ab6354472793e5
wentam <wentam42@gmail.com>
2014-02-18 16:56:22 -0500

    Look in the correct direction, from top-down, assuming that Z is up and down

--- src/com/wentam/graffiks/GraffiksRenderer.java
+++ src/com/wentam/graffiks/GraffiksRenderer.java
@@ -1,17 +1,17 @@
 //////////////////////////////////////////////////////////////////////////////
 // Copyright 2012 Matthew Egeler
-// 									       
-// Licensed under the Apache License, Version 2.0 (the "License");	       
-// you may not use this file except in compliance with the License.	       
-// You may obtain a copy of the License at				      
-// 									       
-//     http://www.apache.org/licenses/LICENSE-2.0			       
-// 									       
-// Unless required by applicable law or agreed to in writing, software      
-// distributed under the License is distributed on an "AS IS" BASIS,	       
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and      
-// limitations under the License.					      
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //////////////////////////////////////////////////////////////////////////////
 
 package com.wentam.graffiks;
@@ -49,7 +49,7 @@ public class GraffiksRenderer implements GLSurfaceView.Renderer {
     // vars declared here to avoid GCs
     private long startTime;
     private long endTime;
-    private long dt;   
+    private long dt;
     private int ms_per_frame;
 
     private float ambientColor[] = {0f,0f,0f,1f};
@@ -89,7 +89,7 @@ public class GraffiksRenderer implements GLSurfaceView.Renderer {
 
 	// draw
 	GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-	Matrix.setLookAtM(viewMatrix, 0, 0, 0, -20, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+	Matrix.setLookAtM(viewMatrix, 0, 0, 0, 20, 0f, 0f, 0f, 0f, 1f, 0f);
 
 	view.draw();
 
