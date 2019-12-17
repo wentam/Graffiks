7a56c154f9c48c512c2e73ed7638b2beb44679c4
wentam <wentam42@gmail.com>
2019-12-17 04:17:25 -0500

    YOINK

--- include/graffiks/graffiks.h
+++ include/graffiks/graffiks.h
@@ -3,61 +3,13 @@
 #ifndef GFKS_DRIVER_H
 #define GFKS_DRIVER_H
 
-#ifndef DLL_EXPORT
-#ifdef _WIN32
-#ifdef GRAFFIKS_BUILD_SHARED
-#define DLL_EXPORT __declspec(dllexport)
-#else
-#define DLL_EXPORT __declspec(dllimport)
-#endif
-#else
-#define DLL_EXPORT
-#endif
-#endif
-
 #include <stdbool.h>
+#include <stdint.h>
 
-#ifdef _WIN32
-#include "graffiks/dt_loop.h"
-#include "graffiks/dt_callbacks.h"
-#include <windows.h>
+#ifdef GFKS_CONFIG_X11_SUPPORT
+#include <X11/Xlib.h>
 #endif
 
-#ifdef LINUX
-#include <GL/glx.h>
-#endif
-
-#ifdef _WIN32
-HDC hdc;
-#endif
-
-#ifdef LINUX
-Display *display;
-Window win;
-#endif
-
-DLL_EXPORT void gfks_init_dt(int window_width, int window_height, char *window_title,
-                             void (*init)(int *width, int *height),
-                             void (*update)(float time_step), void (*finish)(void));
-
-#ifdef LINUX
-DLL_EXPORT void gfks_init_with_window(Display *display, Window window);
-DLL_EXPORT void gfks_init_with_window_dt(Display *display, Window window,
-                                         void (*init)(int *width, int *height),
-                                         void (*update)(float time_step), void (*finish)(void));
-#endif
-
-#ifdef _WIN32
-// windows version of init_with_window and init_with_window_dt
-#endif
-
-DLL_EXPORT void gfks_init(int window_width, int window_height, char *window_title);
-
-DLL_EXPORT void gfks_use_vsync(int vsync);
-
-DLL_EXPORT void gfks_set_antialiasing_samples(int samples);
-
-// ----------------------------- old stuff above (deprecated) ------------------
 // ----------------------------- new stuff below -------------------------------
 
 #define GFKS_ENGINE_NAME "Graffiks"
